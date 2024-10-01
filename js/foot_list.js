14390ec1de0cfe3217ba0ec73ee10a3b9ae7679e
LeeYongSeok13 <minamiharuka333@gmail.com>
2024-10-01 13:47:59 +0900

    particuler page add javascript

--- js/foot_list.js
+++ js/foot_list.js
@@ -6,7 +6,7 @@ function createProductData() {
       name: "아이배냇 한끼 영양밥 쇠고기시금치150g x 6",
       description:
         "영양 가득 소고기와 시금치로 맛있는 한 끼가 뚝딱 해결되고 국내산 원재료와 우리 콩 간장, 올리고당으로 더욱 맛있는 3세 이상 아이부터 어른까지 맛있게 드실 수 있는 한 끼 영양밥입니다.",
-      price: "11,900원",
+      price: "1,1900원",
       category: "식료품",
     },
     {
@@ -15,7 +15,7 @@ function createProductData() {
       name: "동원 들깨 칼국수 258g x 4",
       description:
         "사골육수에 껍질을 제거한 들깨가루를 첨가한 부드러운 국물 맛의 들깨 칼국수입니다.",
-      price: "11,990원",
+      price: "1,1990원",
       category: "식료품",
     },
     {
@@ -23,7 +23,7 @@ function createProductData() {
       image: "image3.webp",
       name: "동원 양반 차돌 육개장 460g x 5",
       description: "재료가 살아 있는 동원F&B 양반 차돌 육개장입니다.",
-      price: "12,490원",
+      price: "1,2490원",
       category: "식료품",
     },
     {
@@ -31,7 +31,7 @@ function createProductData() {
       image: "image4.webp",
       name: "동원 양반 백합 미역국 460g x 5",
       description: "재료가 살아 있는 동원F&B 양반 백합미역국입니다.",
-      price: "12,490원",
+      price: "1,2490원",
       category: "식료품",
     },
     {
@@ -40,7 +40,7 @@ function createProductData() {
       name: "동원 면발의신 해물칼국수 236g x 4",
       description:
         "멸치와 바지락으로 시원한 육수의 맛을 살린 해물칼국수입니다.",
-      price: "13,990원",
+      price: "1,3990원",
       category: "식료품",
     },
     {
@@ -48,7 +48,7 @@ function createProductData() {
       image: "image6.webp",
       name: "Apple 맥북 에어 15",
       description:
-        "놀랍도록  얇고 엄청나게 빠른 MacBook Air를 이제 두 가지 사이즈와 네 가지 컬러로 만나볼 수 있습니다. 당시에게 꼭 맞는 Air를 선택하세요.",
+        "놀랍도록 얇고 엄청나게 빠른 MacBook Air를 이제 두 가지 사이즈와 네 가지 컬러로 만나볼 수 있습니다. 당시에게 꼭 맞는 Air를 선택하세요.",
       price: "1,609,000원",
       category: "가전",
     },
@@ -86,18 +86,14 @@ function createProductData() {
       category: "가전",
     },
   ];
-
   localStorage.setItem("productList", JSON.stringify(list));
 }
 createProductData();
 const products = JSON.parse(localStorage.getItem("productList"));
-
 let ProductSlider = document.querySelector(".ProductSlider");
-
 for (let i = 0; i < products.length; i++) {
   // 요소 생성
   const divBox = document.createElement("div");
-
   const img = document.createElement("img");
   img.setAttribute("src", `../img/${products[i].image}`);
   //요소.classlist.add(클래스명)
@@ -109,10 +105,11 @@ for (let i = 0; i < products.length; i++) {
   divBox4.innerHTML = products[i].price;
   const divBox5 = document.createElement("div");
   divBox5.innerHTML = products[i].category;
-
   divBox.append(img, divBox2, divBox3, divBox4, divBox5);
+  divBox.addEventListener("click", function () {
+    window.location.href = `particuler_page.html?id=${products[i].id}`;
+  });
   console.log(divBox);
   ProductSlider.append(divBox);
-
   // 자식요소 추가 (요소.append(추가할 요소 ))
 }
--- js/particuler_page.js
+++ js/particuler_page.js
@@ -7,11 +7,23 @@ const productId = parseInt(urlParams.get(`id`), 10);
 
 function renderProductDetail(product) {
   const productDetail = document.getElementById("product-detail");
-  productDetail.innerHTML = `<img src="${products.image}" alt="${products.name}">
-  <h2>${products.name}</h2>
-  <p>가격 : ${products.price.toLocaleString}원</p>
-  <p>${products.description}</p>
-  <button onclick="addToCart(${products.id})">장바구니</button>`;
+  productDetail.innerHTML = `<div class="p-d-left"><img src="../img/${product.image}" alt="${product.name}"></div>
+  </div>
+  <div class="p-d-right">
+  <h2>${product.name}</h2>
+  <span>${product.description}</span>
+  <p>가격 : ${product.price}</p>
+    <div class="quantity-container">
+    <button id="decrease">-</button>
+    <input type="text" id="quantity" value="1" readonly>
+    <button id="increase">+</button>
+  </div>
+
+  <div class="total-price">
+    총 금액: <span id="total">${product.price}</span>
+  </div>
+  <button onclick="addToCart(${product.id})">장바구니</button>
+  </div>`;
 }
 
 // 페이지 로드 시 상품 이미지, 정보 렌더링
@@ -19,7 +31,7 @@ function renderProductDetail(product) {
 window.onload = () => {
   const product = products.find((p) => p.id === productId);
   if (product) {
-    renderProductImg(product);
+    // renderProductImg(product);
     renderProductDetail(product);
   } else {
     document.getElementById("product-detail").innerHTML =
